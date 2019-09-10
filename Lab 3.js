77524667a68f7030b0323a03e6df6998eb04b928
happycooldude <pieterhuisman2000@gmail.com>
2019-09-10 12:03:43 +0200

    tafel van 3 toegevoegd + de rekenvraag

--- Lab 3.js
+++ Lab 3.js
@@ -3,5 +3,13 @@ var b = 6
 var c = 3
 
 for(a = 0; a<=10; a++){
-  document.write(a,"X6= " + a*b + "<br>")
+  document.write(a,"X6= " + a*b + "<br>");
 }
+
+document.write("<br>");
+
+for(a = 0; a<=10; a++){
+  document.write(a,"X3= " + a*c + "<br>");
+}
+
+document.write("<br>" , "12+6x10/5-12 =" , 12+6*10/5-12)
