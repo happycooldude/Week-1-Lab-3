f3bf64d78e697185e5774cd2c2045e26cd920ce6
happycooldude <pieterhuisman2000@gmail.com>
2019-09-10 14:09:32 +0200

    opdracht 1,2,3 en 4 gemaakt

--- Lab 3.js
+++ Lab 3.js
@@ -1,6 +1,7 @@
 var a = 1;
-var b = prompt("Vul hier het eerste getal in waarvan u de tafel wilt weten")
-var c = prompt("Vul hier het tweede getal in waarvan u de tafel wilt weten")
+var b = prompt("Vul hier het eerste getal in waarvan u de tafel wilt weten");
+var c = prompt("Vul hier het tweede getal in waarvan u de tafel wilt weten");
+
 
 for(a = 1; a<=10; a++){
   document.write(a,"X",b,"=" + a*b + "<br>");
@@ -12,4 +13,8 @@ for(a = 1; a<=10; a++){
   document.write(a,"X",c,"=" + a*c + "<br>");
 }
 
-document.write("<br>" , "12+6x10/5-12 =" , 12+6*10/5-12)
+var d = prompt("vul hier een getal die u zelf wilt");
+var e = prompt("vul hier een getal die u zelf wilt");
+var f = prompt("vul hier een getal die u zelf wilt");
+
+document.write("<br>" , "12","+",d,"x",e,"/",f,"-","12 = " , 12+d*e/f-12);
