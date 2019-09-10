a4a34232d48ced742b56cf7f56c9efbc5533295e
happycooldude <pieterhuisman2000@gmail.com>
2019-09-10 11:52:36 +0200

    for loop verbeterd

--- Lab 3.js
+++ Lab 3.js
@@ -3,9 +3,5 @@ var b = 6
 var c = 3
 
 for(a = 0; a<=10; a++){
-  document.write(a*b + "<br>")
-}
-
-for(a = 0; a<=10; a++){
-  document.write(a*c + "<br>")
+  document.write(a,"X6= " + a*b + "<br>")
 }
