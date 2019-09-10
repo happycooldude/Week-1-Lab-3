12a42ddd5cf4268ae1680bcbf24fc418d3328a4e
happycooldude <pieterhuisman2000@gmail.com>
2019-09-10 12:30:50 +0200

    Gebruikers input toegevoegd

--- Lab 3.js
+++ Lab 3.js
@@ -1,15 +1,15 @@
-var a = 0
-var b = 6
-var c = 3
+var a = 0;
+var b = prompt("Vul hier het eerste getal in waarvan u de tafel wilt weten")
+var c = prompt("Vul hier het tweede getal in waarvan u de tafel wilt weten")
 
 for(a = 0; a<=10; a++){
-  document.write(a,"X6= " + a*b + "<br>");
+  document.write(a,"X",b,"=" + a*b + "<br>");
 }
 
 document.write("<br>");
 
 for(a = 0; a<=10; a++){
-  document.write(a,"X3= " + a*c + "<br>");
+  document.write(a,"X",c,"=" + a*c + "<br>");
 }
 
 document.write("<br>" , "12+6x10/5-12 =" , 12+6*10/5-12)
