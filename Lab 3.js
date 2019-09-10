be7a176bb5861593d567aa8d9a3aee432314a434
happycooldude <pieterhuisman2000@gmail.com>
2019-09-10 14:37:18 +0200

    kleine aanpassingen gemaakt

--- Lab 3.js
+++ Lab 3.js
@@ -2,7 +2,6 @@ var a = 1;
 var b = prompt("Vul hier het eerste getal in waarvan u de tafel wilt weten");
 var c = prompt("Vul hier het tweede getal in waarvan u de tafel wilt weten");
 
-
 for(a = 1; a<=10; a++){
   document.write(a,"X",b,"=" + a*b + "<br>");
 }
@@ -13,8 +12,15 @@ for(a = 1; a<=10; a++){
   document.write(a,"X",c,"=" + a*c + "<br>");
 }
 
-var d = prompt("vul hier een getal die u zelf wilt");
-var e = prompt("vul hier een getal die u zelf wilt");
-var f = prompt("vul hier een getal die u zelf wilt");
+var getal = 12;
+var d = prompt("Vul hier een vrijwillig getal in")
+var e = prompt("Vul ook hier een vrijwillig getal in")
+var f = prompt("En hier")
+var g = prompt("Laatste keer")
 
-document.write("<br>" , "12","+",d,"x",e,"/",f,"-","12 = " , 12+d*e/f-12);
+getal = getal + d;
+getal = getal * e;
+getal = getal / f;
+getal = getal - g;
+
+document.write("<br>",getal);
